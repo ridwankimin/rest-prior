1a6b8fee0b9e51c227a500e09e0a9e5049dcc621
ridwankimin <mochamadridwan008@gmail.com>
2024-08-02 21:02:45 +0700

    update dikit

--- application/controllers/SendPermohonan.php
+++ application/controllers/SendPermohonan.php
@@ -39,86 +39,87 @@ class SendPermohonan extends RestController
         $this->form_validation->set_rules('STATUS', 'Status', 'required|alpha');
 
         if ($this->form_validation->run() == FALSE) {
-            $errors = $this->form_validation->error_array();
-            $this->response(['status' => FALSE, 'errors' => $errors], RESTController::HTTP_BAD_REQUEST);
-            return;
-        }
-
-        // Validate HEADER.PERUSAHAAN
-        if (isset($data['DOKUMEN']['HEADER']['PERUSAHAAN'])) {
-            $this->form_validation->set_data($data['DOKUMEN']['HEADER']['PERUSAHAAN']);
-            $this->form_validation->set_rules('ID', 'ID PERUSAHAAN', 'required|alpha_numeric');
-            $this->form_validation->set_rules('NAMA', 'Nama PERUSAHAAN', 'required');
-            $this->form_validation->set_rules('ALAMAT', 'Alamat PERUSAHAAN', 'required');
-            $this->form_validation->set_rules('KOTA', 'Kota PERUSAHAAN', 'required');
-            $this->form_validation->set_rules('PROVINSI', 'Provinsi PERUSAHAAN', 'required');
-            $this->form_validation->set_rules('JENIS', 'Jenis PERUSAHAAN', 'required|alpha');
-            $this->form_validation->set_rules('JNSAPI', 'Jenis API PERUSAHAAN', 'required|numeric');
-
-            if ($this->form_validation->run() == FALSE) {
-                $errors = $this->form_validation->error_array();
-                $this->response(['status' => FALSE, 'errors' => $errors], RESTController::HTTP_BAD_REQUEST);
-                return;
-            }
-        }
-
-        // Validate HEADER.PERUSAHAAN.PJAWAB
-        if (isset($data['DOKUMEN']['HEADER']['PERUSAHAAN']['PJAWAB'])) {
-            $this->form_validation->set_data($data['DOKUMEN']['HEADER']['PERUSAHAAN']['PJAWAB']);
-            $this->form_validation->set_rules('NAMA', 'Nama Penanggung Jawab', 'required');
-            $this->form_validation->set_rules('JABATAN', 'Jabatan Penanggung Jawab', 'required');
-            $this->form_validation->set_rules('ALAMAT', 'Alamat Penanggung Jawab', 'required');
-            $this->form_validation->set_rules('KOTA', 'Kota Penanggung Jawab', 'required');
-            $this->form_validation->set_rules('EMAIL', 'Email Penanggung Jawab', 'required|valid_email');
-
-            if ($this->form_validation->run() == FALSE) {
-                $errors = $this->form_validation->error_array();
-                $this->response(['status' => FALSE, 'errors' => $errors], RESTController::HTTP_BAD_REQUEST);
-                return;
-            }
-        }
-        
-        if($data['DOKUMEN']['JNSPERMOHONAN']=='03999') $kar='Hewan';
-        if($data['DOKUMEN']['JNSPERMOHONAN']=='04999') $kar='Tumbuhan';
-        if($data['DOKUMEN']['JNSPERMOHONAN']=='02999') $kar='Ikan';
-        
-        $input = array(
-            'id' => $data['DOKUMEN']['IDPERMOHONAN'],
-            'noAju' => $data['DOKUMEN']['NOPERMOHONAN'],
-            'tglAju' => $data['DOKUMEN']['WKPERMOHONAN'],
-            'jnsAju' => $data['DOKUMEN']['JNSPERMOHONAN'],
-            'nmPerusahaan' => $data['DOKUMEN']['HEADER']['PERUSAHAAN']['NAMA'],
-            'npwp' => $data['DOKUMEN']['HEADER']['PERUSAHAAN']['ID'],
-            'ppjk' => $data['DOKUMEN']['HEADER']['PPJK']['NMPPJK'],
-            'npwp_ppjk' => $data['DOKUMEN']['HEADER']['PPJK']['IDPPJK'],
-            // 'noReg' => $data['DOKUMEN']['IDPERMOHONAN'],
-            'jenis_karantina' => $kar,
-            'upt' => $data['DOKUMEN']['HEADER']['GA']['KARANTINA']['UPT'],
-            'modaAngkut' => $data['DOKUMEN']['HEADER']['ANGKUT']['MODA'],
-            'namaAngkut' => $data['DOKUMEN']['HEADER']['ANGKUT']['NMANGKUT'],
-            // 'gudang' => $data['DOKUMEN']['IDPERMOHONAN'],
-            // 'kd_tpk' => $data['DOKUMEN']['IDPERMOHONAN'],
-            'tglTiba' => $data['DOKUMEN']['HEADER']['ANGKUT']['TGTIBA'],
-            'portAsal' => $data['DOKUMEN']['HEADER']['PELABUHAN']['ASAL'],
-            'portTujuan' => $data['DOKUMEN']['HEADER']['PELABUHAN']['BONGKAR'],
-            // 'tujuan' => $data['DOKUMEN']['IDPERMOHONAN'],
-            // 'peruntukan' => $data['DOKUMEN']['IDPERMOHONAN'],
-            'xml' => $json_data
-        );
-        
-        $result = $this->permohonan->insert($input);
-        var_dump($result);
-        if($result>0) {
-            $this->response([
-                'status' => TRUE,
-                'message' => 'Sukses insert data'
-            ], RESTController::HTTP_CREATED);
-
-        } else {
             $this->response([
                 'status' => FALSE,
-                'message' => 'Gagal Insert Data'
+                'message' => validation_errors()
             ], RESTController::HTTP_BAD_REQUEST);
+        } else {
+            // Validate HEADER.PERUSAHAAN
+            if (isset($data['DOKUMEN']['HEADER']['PERUSAHAAN'])) {
+                $this->form_validation->set_data($data['DOKUMEN']['HEADER']['PERUSAHAAN']);
+                $this->form_validation->set_rules('ID', 'ID PERUSAHAAN', 'required|alpha_numeric');
+                $this->form_validation->set_rules('NAMA', 'Nama PERUSAHAAN', 'required');
+                $this->form_validation->set_rules('ALAMAT', 'Alamat PERUSAHAAN', 'required');
+                $this->form_validation->set_rules('KOTA', 'Kota PERUSAHAAN', 'required');
+                $this->form_validation->set_rules('PROVINSI', 'Provinsi PERUSAHAAN', 'required');
+                $this->form_validation->set_rules('JENIS', 'Jenis PERUSAHAAN', 'required|alpha');
+                $this->form_validation->set_rules('JNSAPI', 'Jenis API PERUSAHAAN', 'required|numeric');
+    
+                if ($this->form_validation->run() == FALSE) {
+                    $errors = $this->form_validation->error_array();
+                    $this->response(['status' => FALSE, 'errors' => $errors], RESTController::HTTP_BAD_REQUEST);
+                    return;
+                }
+            }
+    
+            // Validate HEADER.PERUSAHAAN.PJAWAB
+            if (isset($data['DOKUMEN']['HEADER']['PERUSAHAAN']['PJAWAB'])) {
+                $this->form_validation->set_data($data['DOKUMEN']['HEADER']['PERUSAHAAN']['PJAWAB']);
+                $this->form_validation->set_rules('NAMA', 'Nama Penanggung Jawab', 'required');
+                $this->form_validation->set_rules('JABATAN', 'Jabatan Penanggung Jawab', 'required');
+                $this->form_validation->set_rules('ALAMAT', 'Alamat Penanggung Jawab', 'required');
+                $this->form_validation->set_rules('KOTA', 'Kota Penanggung Jawab', 'required');
+                $this->form_validation->set_rules('EMAIL', 'Email Penanggung Jawab', 'required|valid_email');
+    
+                if ($this->form_validation->run() == FALSE) {
+                    $errors = $this->form_validation->error_array();
+                    $this->response(['status' => FALSE, 'errors' => $errors], RESTController::HTTP_BAD_REQUEST);
+                    return;
+                }
+            }
+            
+            if($data['DOKUMEN']['JNSPERMOHONAN']=='03999') $kar='Hewan';
+            if($data['DOKUMEN']['JNSPERMOHONAN']=='04999') $kar='Tumbuhan';
+            if($data['DOKUMEN']['JNSPERMOHONAN']=='02999') $kar='Ikan';
+            
+            $input = array(
+                'id' => $data['DOKUMEN']['IDPERMOHONAN'],
+                'noAju' => $data['DOKUMEN']['NOPERMOHONAN'],
+                'tglAju' => $data['DOKUMEN']['WKPERMOHONAN'],
+                'jnsAju' => $data['DOKUMEN']['JNSPERMOHONAN'],
+                'nmPerusahaan' => $data['DOKUMEN']['HEADER']['PERUSAHAAN']['NAMA'],
+                'npwp' => $data['DOKUMEN']['HEADER']['PERUSAHAAN']['ID'],
+                'ppjk' => $data['DOKUMEN']['HEADER']['PPJK']['NMPPJK'],
+                'npwp_ppjk' => $data['DOKUMEN']['HEADER']['PPJK']['IDPPJK'],
+                // 'noReg' => $data['DOKUMEN']['IDPERMOHONAN'],
+                'jenis_karantina' => $kar,
+                'upt' => $data['DOKUMEN']['HEADER']['GA']['KARANTINA']['UPT'],
+                'modaAngkut' => $data['DOKUMEN']['HEADER']['ANGKUT']['MODA'],
+                'namaAngkut' => $data['DOKUMEN']['HEADER']['ANGKUT']['NMANGKUT'],
+                // 'gudang' => $data['DOKUMEN']['IDPERMOHONAN'],
+                // 'kd_tpk' => $data['DOKUMEN']['IDPERMOHONAN'],
+                'tglTiba' => $data['DOKUMEN']['HEADER']['ANGKUT']['TGTIBA'],
+                'portAsal' => $data['DOKUMEN']['HEADER']['PELABUHAN']['ASAL'],
+                'portTujuan' => $data['DOKUMEN']['HEADER']['PELABUHAN']['BONGKAR'],
+                // 'tujuan' => $data['DOKUMEN']['IDPERMOHONAN'],
+                // 'peruntukan' => $data['DOKUMEN']['IDPERMOHONAN'],
+                'xml' => $json_data
+            );
+            
+            $result = $this->permohonan->insert($input);
+            var_dump($result);
+            if($result>0) {
+                $this->response([
+                    'status' => TRUE,
+                    'message' => 'Sukses insert data'
+                ], RESTController::HTTP_CREATED);
+    
+            } else {
+                $this->response([
+                    'status' => FALSE,
+                    'message' => 'Gagal Insert Data'
+                ], RESTController::HTTP_BAD_REQUEST);
+            }
         }
     }
 }
